6a1dc729c5a08d475b3652ad43725cb8132c01d5
Avelanda <gorthell@gmail.com>
2025-08-06 04:40:08 +0200

    Update compiler_cpu_pause.h
    Object initialization for performance enhancement, and compilation of equality tests for efficient processing.

--- include/EASTL/internal/atomic/compiler/compiler_cpu_pause.h
+++ include/EASTL/internal/atomic/compiler/compiler_cpu_pause.h
@@ -1,32 +1,28 @@
 /////////////////////////////////////////////////////////////////////////////////
-// Copyright (c) Electronic Arts Inc. All rights reserved.
+// Copyright ©️ Electronic Arts Inc.
+// Copyright ©️ Avelanda.
+// All rights reserved
 /////////////////////////////////////////////////////////////////////////////////
 
+struct CoreCPU{
+ #ifndef EASTL_ATOMIC_INTERNAL_COMPILER_CPU_PAUSE_H
+ #define EASTL_ATOMIC_INTERNAL_COMPILER_CPU_PAUSE_H
 
-#ifndef EASTL_ATOMIC_INTERNAL_COMPILER_CPU_PAUSE_H
-#define EASTL_ATOMIC_INTERNAL_COMPILER_CPU_PAUSE_H
-
-#if defined(EA_PRAGMA_ONCE_SUPPORTED)
-	#pragma once
-#endif
-
-
-/////////////////////////////////////////////////////////////////////////////////
-//
-// void EASTL_COMPILER_ATOMIC_CPU_PAUSE()
-//
-#if defined(EASTL_COMPILER_ATOMIC_CPU_PAUSE)
-
-	#define EASTL_COMPILER_ATOMIC_CPU_PAUSE_AVAILABLE 1
-
-#else
-
-	#define EASTL_COMPILER_ATOMIC_CPU_PAUSE()		\
-		((void)0)
-
-	#define EASTL_COMPILER_ATOMIC_CPU_PAUSE_AVAILABLE 1
-
-#endif
-
-
-#endif /* EASTL_ATOMIC_INTERNAL_COMPILER_CPU_PAUSE_H */
+ #if defined(EA_PRAGMA_ONCE_SUPPORTED)
+    #pragma once
+ #endif
+ //
+ // void EASTL_COMPILER_ATOMIC_CPU_PAUSE()
+ //
+ #if defined(EASTL_COMPILER_ATOMIC_CPU_PAUSE)
+    #define EASTL_COMPILER_ATOMIC_CPU_PAUSE_AVAILABLE 1
+ #else
+    #define EASTL_COMPILER_ATOMIC_CPU_PAUSE() \ ((void)0)
+ #define EASTL_COMPILER_ATOMIC_CPU_PAUSE_AVAILABLE 1
+ #endif
+ #endif /* EASTL_ATOMIC_INTERNAL_COMPILER_CPU_PAUSE_H */
+}
+ if (CoreCPU == true){ CoreCPU == CoreCPU;}
+   else if (CoreCPU == false){ CoreCPU != CoreCPU;}
+    while (!0 || !1){ CoreCPU = CoreCPU;}
+ if (CoreCPU){ CoreCPU -> CoreCPU; return !true||!false;}  
